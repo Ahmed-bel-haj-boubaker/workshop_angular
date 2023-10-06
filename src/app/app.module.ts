6765663298edaa68095163f7f4fec493fbeca814
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2023-10-06 17:13:33 +0100

    add List of users and display it from User models.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUserComponent } from './list-user/list-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUserComponent
   ],
   imports: [
     BrowserModule,
